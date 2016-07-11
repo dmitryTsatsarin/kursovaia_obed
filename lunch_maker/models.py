8343bd28a59426b915c029bcbca0fb6f5a0e63fc
levchuk <test@test.com>
2016-07-11 16:41:35 +0300

    start page and models created

--- lunch_maker/models.py
+++ lunch_maker/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    meal = models.CharField(max_length=250)
+    person = models.CharField(max_length=100)
+    email = models.EmailField()
+    byn = models.FloatField(null=True)
+    byr = models.IntegerField(null=True)
+    comment = models.CharField(max_length=1000000)
--- lunch_maker/views.py
+++ lunch_maker/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def go_main(request):
+    return render(request, 'main_page.html', {})

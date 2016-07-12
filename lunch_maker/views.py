b7eb2af836ce0c1f50745bf53ded12f2a414745c
levchuk <test@test.com>
2016-07-12 16:31:59 +0300

    show_page result added

--- lunch_maker/views.py
+++ lunch_maker/views.py
@@ -33,5 +33,15 @@ def new_order(request):
 
 
 def show(request):
-    context = {'data':Order.objects.filter()}
+    data = Order.objects.filter().values()
+    result_byn = 0
+    result_byr = 0
+    for i in data:
+        if i['byn'] is not None:
+            result_byn += i['byn']
+    for i in data:
+        if i['byr'] is not None:
+            result_byr += i['byr']
+    result = result_byr/10000 + result_byn
+    context = {'data':data, 'result_byn':result_byn, 'result_byr':result_byr, 'result':result}
     return render(request, 'show_page.html', context)
